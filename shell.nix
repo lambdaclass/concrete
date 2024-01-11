cc8fdd722f9cdf929f9b73de2390d01747df2aba
Esteve Soler Arderiu <esteve.soler@lambdaclass.com>
2024-01-11 13:11:01 +0100

    Add `shell.nix`.

---
+++ shell.nix
@@ -0,0 +1,16 @@
+{ nixpkgs ? import <nixpkgs> {} }:
+
+let
+  pinnedPkgs = builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/23.05.tar.gz";
+    sha256 = "10wn0l08j9lgqcw8177nh2ljrnxdrpri7bp0g7nvrsn9rkawvlbf";
+  };
+  pkgs = import pinnedPkgs {};
+in
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    pkgs.cargo
+    pkgs.rustc
+  ];
+}
