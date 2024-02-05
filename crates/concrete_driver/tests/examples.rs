d207c3a30eb66a0376b0edd2230b35254bc774de
Edgar Luque <git@edgarluque.com>
2024-02-05 12:15:12 +0100

    tests

--- crates/concrete_driver/tests/examples.rs
+++ crates/concrete_driver/tests/examples.rs
@@ -1,77 +1,24 @@
 use crate::common::{compile_program, run_program};
+use test_case::test_case;
 
 mod common;
 
-#[test]
-fn borrow() {
-    let program = compile_program(
-        include_str!("../../../examples/borrow.con"),
-        "borrow",
-        false,
-    )
-    .unwrap();
+#[test_case(include_str!("../../../examples/borrow.con"), "borrow", false, 2 ; "borrow.con")]
+#[test_case(include_str!("../../../examples/factorial_if.con"), "factorial_if", false, 24 ; "factorial_if.con")]
+#[test_case(include_str!("../../../examples/fib_if.con"), "fib_if", false, 55 ; "fib_if.con")]
+#[test_case(include_str!("../../../examples/import.con"), "import", false, 12 ; "import.con")]
+#[test_case(include_str!("../../../examples/simple.con"), "simple", false, 8 ; "simple.con")]
+#[test_case(include_str!("../../../examples/while.con"), "while", false, 16 ; "while.con")]
+#[test_case(include_str!("../../../examples/chars.con"), "chars", false, 117 ; "chars.con")]
+#[test_case(include_str!("../../../examples/floats.con"), "floats", false, 1 ; "floats.con")]
+fn example_tests(source: &str, name: &str, is_library: bool, status_code: i32) {
+    let program = compile_program(source, name, is_library).unwrap();
 
     let result = run_program(&program.binary_file).unwrap();
-    assert_eq!(result.status.code().unwrap(), 2);
-}
-
-#[test]
-fn factorial_if() {
-    let program = compile_program(
-        include_str!("../../../examples/factorial_if.con"),
-        "factorial_if",
-        false,
-    )
-    .unwrap();
-
-    let result = run_program(&program.binary_file).unwrap();
-    assert_eq!(result.status.code().unwrap(), 24);
-}
-
-#[test]
-fn fib_if() {
-    let program = compile_program(
-        include_str!("../../../examples/fib_if.con"),
-        "fib_if",
-        false,
-    )
-    .unwrap();
-
-    let result = run_program(&program.binary_file).unwrap();
-    assert_eq!(result.status.code().unwrap(), 55);
-}
-
-#[test]
-fn import() {
-    let program = compile_program(
-        include_str!("../../../examples/import.con"),
-        "import",
-        false,
-    )
-    .unwrap();
-
-    let result = run_program(&program.binary_file).unwrap();
-    assert_eq!(result.status.code().unwrap(), 12);
-}
-
-#[test]
-fn simple() {
-    let program = compile_program(
-        include_str!("../../../examples/simple.con"),
-        "simple",
-        false,
-    )
-    .unwrap();
-
-    let result = run_program(&program.binary_file).unwrap();
-    assert_eq!(result.status.code().unwrap(), 8);
-}
-
-#[test]
-fn r#while() {
-    let program =
-        compile_program(include_str!("../../../examples/while.con"), "while", false).unwrap();
-
-    let result = run_program(&program.binary_file).unwrap();
-    assert_eq!(result.status.code().unwrap(), 16);
+    assert_eq!(
+        result.status.code().unwrap(),
+        status_code,
+        "Program {} returned a unexpected status code",
+        name
+    );
 }
