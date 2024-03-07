a1c90da486d04bb33fe90886f7c8a98e5ac2ede6
Edgar Luque <git@edgarluque.com>
2024-03-07 17:37:31 +0100

    bench

--- bench/bench.c
+++ bench/bench.c
@@ -1,21 +1,70 @@
 #include <assert.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
-extern uint64_t factorial_concrete(uint64_t n);
-extern uint64_t factorial_rust(uint64_t n);
+extern uint64_t concrete_function(uint64_t n);
+extern uint64_t rust_function(uint64_t n);
 
-int main() {
+struct timespec timer_start() {
+  struct timespec start_time;
+  clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start_time);
+  return start_time;
+}
 
-  clock_t begin = clock();
-  uint64_t result_concrete = factorial_concrete(20);
-  clock_t end = clock();
-  double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+long timer_end(struct timespec start_time) {
+  struct timespec end_time;
+  clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end_time);
+  long diffInNanos = (end_time.tv_sec - start_time.tv_sec) * (long)1e9 +
+                     (end_time.tv_nsec - start_time.tv_nsec);
+  return diffInNanos;
+}
 
-  clock_t begin2 = clock();
-  uint64_t result_rust = factorial_rust(20);
-  clock_t end2 = clock();
-  double time_spent2 = (double)(end - begin) / CLOCKS_PER_SEC;
+int main(int argc, const char **argv) {
+  if (argc < 2) {
+    fprintf(stderr, "missing iteration arguments\n");
+    return 1;
+  }
+
+  if (argc < 3) {
+    fprintf(stderr, "missing input number argument\n");
+    return 1;
+  }
+
+  int num_iters = atoi(argv[1]);
+  int input = atoi(argv[2]);
+
+  uint64_t result_concrete;
+  uint64_t result_rust;
+
+  // warmup + sanity check
+  for (size_t i = 0; i < 3; ++i) {
+    assert(concrete_function(input) == rust_function(input));
+  }
+
+  printf("Running %d iterations\n", num_iters);
+  printf("Using input value:\t%d\n", input);
+
+  {
+    struct timespec vartime = timer_start();
+    for (size_t i = 0; i < num_iters; ++i) {
+      result_concrete = concrete_function(input);
+    }
+    long time_elapsed_nanos = timer_end(vartime);
+    printf("Concrete Result =\t%lu\t\tTime taken : %.2Lf ms\n", result_concrete,
+           (long double)time_elapsed_nanos / 1000000.0L);
+  }
+
+  {
+    struct timespec vartime = timer_start();
+    for (size_t i = 0; i < num_iters; ++i) {
+      result_rust = rust_function(input);
+    }
+    long time_elapsed_nanos = timer_end(vartime);
+    printf("Rust Result =\t\t%lu\t\tTime taken : %.2Lf ms\n", result_rust,
+           (long double)time_elapsed_nanos / 1000000.0L);
+  }
 
   assert(result_concrete == result_rust);
 
--- bench/factorial_if.rs
+++
@@ -1,8 +0,0 @@
-#[no_mangle]
-pub extern "C" fn factorial_rust(n: u64) -> u64 {
-    if n == 0 {
-        return 1;
-    } else {
-        return n * factorial_rust(n - 1);
-    }
-}
