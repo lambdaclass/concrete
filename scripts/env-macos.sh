377e44603b44c475901e86df911e7c79b08000a2
Edgar Luque <git@edgarluque.com>
2024-05-06 12:53:53 +0200

    upd env-macos.sh

--- scripts/env-macos.sh
+++ scripts/env-macos.sh
@@ -3,10 +3,10 @@
 # This script is only useful on macOS using brew.
 # It sets the LLVM environment variables.
 
-MLIR_SYS_170_PREFIX="$(brew --prefix llvm@17)"
-LLVM_SYS_170_PREFIX="$(brew --prefix llvm@17)"
-TABLEGEN_170_PREFIX="$(brew --prefix llvm@17)"
+MLIR_SYS_180_PREFIX="$(brew --prefix llvm@18)"
+LLVM_SYS_180_PREFIX="$(brew --prefix llvm@18)"
+TABLEGEN_180_PREFIX="$(brew --prefix llvm@18)"
 
-export MLIR_SYS_170_PREFIX
-export LLVM_SYS_170_PREFIX
-export TABLEGEN_170_PREFIX
+export MLIR_SYS_180_PREFIX
+export LLVM_SYS_180_PREFIX
+export TABLEGEN_180_PREFIX
