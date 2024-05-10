b19ba577f0f7c43ece55f21a18fe31ee8580f95b
Edgar <git@edgarluque.com>
2024-05-10 14:07:06 +0100

    docs

--- crates/concrete_session/src/lib.rs
+++ crates/concrete_session/src/lib.rs
@@ -9,15 +9,25 @@ pub mod config;
 /// like whether to generate debug info, optimization levels, target, host, etc.
 #[derive(Debug, Clone)]
 pub struct Session {
+    /// The file paths of the included sources from the initial compile unit.
     pub file_paths: Vec<PathBuf>,
+    /// Whether to output debug info.
     pub debug_info: DebugInfo,
+    /// The optimization level to use with this compilation unit.
     pub optlevel: OptLevel,
-    pub sources: Vec<Source<String>>, // for debugging locations
+    /// Sources for debugging locations.
+    pub sources: Vec<Source<String>>,
     /// True if it should be compiled as a library false for binary.
     pub library: bool,
+    /// The file where to put the compilation result.
+    /// The file name will be used for all the other options, if it's
+    /// a library, the platform extension will be added.
     pub output_file: PathBuf,
+    /// Whether to output the generated MLIR file for this compile unit.
     pub output_mlir: bool,
+    /// Whether to output the generated LLVM IR file for this compile unit.
     pub output_ll: bool,
+    /// Whether to output the generated assembly file for this compile unit.
     pub output_asm: bool,
     // todo: include target, host, etc
 }
