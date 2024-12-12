121b77482c119a40a72127c881b3accec734432a
Edgar Luque <git@edgarluque.com>
2024-12-12 15:51:12 -0300

    Fix eprint

--- crates/concrete_parser/src/db.rs
+++ crates/concrete_parser/src/db.rs
@@ -10,6 +10,6 @@ pub struct DatabaseImpl {
 impl salsa::Database for DatabaseImpl {
     fn salsa_event(&self, event: &dyn Fn() -> salsa::Event) {
         let event = event();
-        eprintln!("Event: {event:?}");
+        tracing::debug!("Salsa Db Event: {event:?}");
     }
 }
