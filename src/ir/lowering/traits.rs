28c69b8553d5ab1f95fac7d998866960ccf048a6
Edgar Luque <git@edgl.dev>
2025-03-27 15:48:00 +0100

    better

--- src/ir/lowering/traits.rs
+++ src/ir/lowering/traits.rs
@@ -10,19 +10,15 @@ use crate::{
 
 pub type TraitIdx = StandardSlabIndex<Arc<TraitDecl>>;
 
-// usize = TypeIdx
-pub type TraitImpls = HashMap<usize, Vec<TraitImpl>>;
+pub type TraitImpls = HashMap<TypeIndex, Vec<TraitImpl>>;
 
 /// A database to manage traits.
 #[derive(Debug, Clone)]
 pub struct TraitDatabase {
     pub traits: StandardSlab<Arc<TraitDecl>>,
-    // Traitidx to module.
-    pub trait_to_module: HashMap<usize, ModuleIndex>,
-    // usize = module idx
-    pub name_to_trait: HashMap<(usize, String), TraitIdx>,
-    // usize = TraitIdx::to_idx
-    pub implementors: HashMap<usize, TraitImpls>,
+    pub trait_to_module: HashMap<TraitIdx, ModuleIndex>,
+    pub name_to_trait: HashMap<(ModuleIndex, String), TraitIdx>,
+    pub implementors: HashMap<TraitIdx, TraitImpls>,
 }
 
 /// An implementation of a given trait.
@@ -54,27 +50,27 @@ impl TraitDatabase {
         }
     }
 
-    pub fn get_implementors(&self, idx: TraitIdx) -> &HashMap<usize, Vec<TraitImpl>> {
-        &self.implementors[&idx.to_idx()]
+    pub fn get_implementors(&self, idx: TraitIdx) -> &TraitImpls {
+        &self.implementors[&idx]
     }
 
     pub fn add_trait(&mut self, tr: Arc<TraitDecl>, module_idx: ModuleIndex) -> TraitIdx {
         debug!("Adding trait: {:?}", tr.name.name);
         let name = tr.name.name.clone();
         let idx = self.traits.insert(tr);
-        self.name_to_trait.insert((module_idx.to_idx(), name), idx);
-        self.trait_to_module.insert(idx.to_idx(), module_idx);
+        self.name_to_trait.insert((module_idx, name), idx);
+        self.trait_to_module.insert(idx, module_idx);
 
         idx
     }
 
     pub fn get_trait_module_idx(&self, id: TraitIdx) -> ModuleIndex {
-        *self.trait_to_module.get(&id.to_idx()).unwrap()
+        *self.trait_to_module.get(&id).unwrap()
     }
 
     pub fn get_trait_by_name(&self, name: &str, module_id: ModuleIndex) -> Option<TraitIdx> {
         self.name_to_trait
-            .get(&(module_id.to_idx(), name.to_string()))
+            .get(&(module_id, name.to_string()))
             .copied()
     }
 
@@ -84,8 +80,8 @@ impl TraitDatabase {
             "Adding trait impl: name={} generics={:?}",
             &tr.name.name, im.generics
         );
-        let implementors = self.implementors.entry(idx.to_idx()).or_default();
-        let entry = implementors.entry(im.implementor.to_idx()).or_default();
+        let implementors = self.implementors.entry(idx).or_default();
+        let entry = implementors.entry(im.implementor).or_default();
         entry.push(im);
     }
 
@@ -95,8 +91,8 @@ impl TraitDatabase {
         check_trait: TraitIdx,
         generics: &[TraitGeneric],
     ) -> bool {
-        if let Some(t) = self.implementors.get(&check_trait.to_idx()) {
-            if let Some(impls) = t.get(&ty.to_idx()) {
+        if let Some(t) = self.implementors.get(&check_trait) {
+            if let Some(impls) = t.get(&ty) {
                 for im in impls {
                     if im.generics == generics {
                         return true;
