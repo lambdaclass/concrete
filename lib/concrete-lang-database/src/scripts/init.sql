3a301ca87b4b05ae9b46149d2253b3fba03f036a
Esteve Soler Arderiu <azteca1998@users.noreply.github.com>
2025-04-10 16:59:57 +0200

    Database progress.

--- lib/concrete-lang-database/src/scripts/init.sql
+++ lib/concrete-lang-database/src/scripts/init.sql
@@ -1,28 +1,96 @@
 CREATE TABLE AliasItem (
     path TEXT NOT NULL,
 
+    -- TODO: Visibility, docstring, generics...
     target TEXT NOT NULL,
 
     PRIMARY KEY (path)
 ) WITHOUT ROWID, STRICT;
 
+
 CREATE TABLE ConstItem (
     path TEXT NOT NULL,
 
-    type TEXT NOT NULL,
+    -- TODO: Visibility, docstring...
+    type  TEXT NOT NULL,
     value TEXT NOT NULL,
 
     PRIMARY KEY (path)
 ) WITHOUT ROWID, STRICT;
 
--- TODO: CREATE TABLE FuncItem ();
---   - Arguments table (named fields?)
 
--- TODO: CREATE TABLE EnumItem ();
---   - Variants table
+CREATE TABLE FuncItem (
+    path TEXT NOT NULL,
 
--- TODO: CREATE TABLE StructItem ();
---   - Members table (named fields + unnamed fields table)
+    -- TODO: Visibility, docstring, generics...
+    abi      TEXT,
+    extern   INTEGER NOT NULL,
+    ret_type TEXT NOT NULL,
+
+    body BLOB,
+
+    PRIMARY KEY (path)
+) WITHOUT ROWID, STRICT;
+
+CREATE TABLE FuncArg (
+    path TEXT NOT NULL,
+
+    name  TEXT NOT NULL,
+    value TEXT NOT NULL,
+
+    PRIMARY KEY (path, name)
+) WITHOUT ROWID, STRICT;
+
+
+CREATE TABLE EnumItem (
+    path TEXT NOT NULL,
+
+    -- TODO: Visibility, docstring, generics...
+
+    PRIMARY KEY (path)
+) WITHOUT ROWID, STRICT;
+
+CREATE TABLE EnumVariant (
+    path TEXT NOT NULL,
+
+    -- Visibility?
+    name TEXT NOT NULL,
+    type INTEGER NOT NULL,
+
+    PRIMARY KEY (path, name)
+) WITHOUT ROWID, STRICT;
+
+
+CREATE TABLE StructItem (
+    path TEXT NOT NULL,
+
+    -- TODO: Visibility, docstring, generics...
+    type INTEGER NOT NULL,
+
+    PRIMARY KEY (path)
+) WITHOUT ROWID, STRICT;
+
 
 -- TODO: CREATE TABLE UnionItem ();
 --   - Members table (named fields + unnamed fields table)
+CREATE TABLE UnionItem (
+    -- TODO: Visibility, docstring, generics...
+) WITHOUT ROWID, STRICT;
+
+
+-- TODO: CREATE TABLE TraitItem ();
+
+
+CREATE TABLE ItemFields (
+    path TEXT NOT NULL,
+
+    vis   INTEGER NOT NULL,
+    name  TEXT NOT NULL,
+    value TEXT NOT NULL,
+
+    PRIMARY KEY (path, name)
+) WITHOUT ROWID, STRICT;
+
+
+-- CREATE VIEW Items ()
+-- AS SELECT ...;
